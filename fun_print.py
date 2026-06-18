da05442b38317325f07b7936ff06a6c2dc217f90
Alik <alikhatsykov@gmail.com>
2026-06-18 14:46:35 +0300

    attemp-3

---
+++ fun_print.py
@@ -0,0 +1,18 @@
+print('hello my frends')
+print('are you my frend?')
+f = True
+while f:
+    n = input()
+    if n == 'yes':
+        print('It\'s very good!')
+        f = False
+    elif n == 'no':
+        print('whell, go back, we haven\'t the common path')
+        f = False
+    else:
+        print('try agan')
+        
+
+
+
+
